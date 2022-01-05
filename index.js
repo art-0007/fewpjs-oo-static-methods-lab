e8954d2b3089eef60e0295f3fee0092eb6dd778c
Artur <info@aigroupusa.com>
2022-01-05 15:13:52 -0500

    commit tree d38550a549b67c7aab94374c158cdc53d0e53117

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
   //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'-' ']+/g, '');
+  }
+
+  static titleize(string) {
+    const prepositions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let words = string.split(' ')
+    let titleized = words.map(function(word) {
+      if (prepositions.includes(word) && word != words[0]) {
+        return word
+      } else {
+        return Formatter.capitalize(word)
+      }
+    })
+    return titleized.join(' ')
+  }
 }
